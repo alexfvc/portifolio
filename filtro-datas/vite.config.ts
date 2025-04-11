3ba6b18e65f87531af6fab6fa8fa828f3386e0f5
alexfvc <alexfvc@gmail.com>
2025-04-11 01:10:06 -0300

    commit react app

--- filtro-datas/vite.config.ts
+++ filtro-datas/vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',
-  build: {
-    outDir: '../app',
-  },
 })
