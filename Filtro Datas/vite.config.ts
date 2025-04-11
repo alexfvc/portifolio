9fa0d10da5590e1c50488d67440a50382b5de413
alexfvc <alexfvc@gmail.com>
2025-04-10 22:19:53 -0300

    add config react - teste 2

--- Filtro Datas/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "portifolio/filtro-datas"
-})
